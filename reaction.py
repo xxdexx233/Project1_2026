9cd0c803f043c0ef8025543dae6ea8727c8962c9
xxdexx233 <1813692108@qq.com>
2026-04-09 15:44:50 +0800

    add name and rewrite the pressed function

--- reaction.py
+++ reaction.py
@@ -6,12 +6,20 @@ led = LED(4)
 right_button = Button(15)
 left_button = Button(14)
 
+left_name = input('left player name is ')
+right_name = input('right player name is ')
+
 led.on()
 sleep(uniform(5,10))
 led.off()
 
 def pressed(button):
-	print(str(button.pin.num)+'won the game')
-
+	if button.pin.number ==14:
+		print(left_name + 'won the game')
+	else:
+		print(right_name + 'won the game')
+	exit()
 right_button.when_pressed = pressed
 left_button.when_pressed = pressed
+
+
