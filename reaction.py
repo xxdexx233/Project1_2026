36e54f9cc551887d91ba3593c342b0413c716f4f
xxdexx233 <1813692108@qq.com>
2026-04-26 15:29:09 +0800

    V2

--- reaction.py
+++ reaction.py
@@ -3,8 +3,11 @@ from time import sleep , time
 from random import uniform
 
 led = LED(4)
-right_button = Button(15)
 left_button = Button(14)
+right_button = Button(15)
+
+start_time =0
+
 
 left_name = input('left player name is ')
 right_name = input('right player name is ')
@@ -13,28 +16,36 @@ left_point=0
 right_point=0
 
 def pressed(button):
-	global left_point,right_point,start_time
+	global left_point,right_point,start_time,left_button,right_button
 	pressed_time = time()
 	reaction_time = round((pressed_time - start_time) * 1000 , 2)
+
+	if led.is_lit:
+		return
+	if(reaction_time<5):
+		return
 	if button.pin.number ==14:
 		print(left_name + ' won the game')
 		left_point+=1
 	else:
-		print(right_name + ' won the game')      
+		print(right_name + ' won the game')
 		right_point+=1
 	print('left_point ' + str(left_point) + ' right_point ' + str(right_point))
 	print('reaction time: '+str(reaction_time))
 	led.on()
 	sleep(uniform(5,10))
 	led.off()
+
 	start_time = time()
-  
+
 right_button.when_pressed = pressed
 left_button.when_pressed = pressed
+
 print('left_point ' + str(left_point) + ' right_point ' + str(right_point))
 led.on()
 sleep(uniform(5,10))
 led.off()
+
 start_time = time()
 
 while(left_point<=5 and right_point<=5):
